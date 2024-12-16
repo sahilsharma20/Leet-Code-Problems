ec980b6d2a6d0223d0ee28921453bd0564324247
sahilsharma20 <sahilsharma68018@gmail.com>
2024-12-16 10:53:18 +0530

    Quick update to Remove Duplicated From Sorted Arrays.cpp

--- Arrays/Remove Duplicated From Sorted Arrays.cpp
+++ Arrays/Remove Duplicated From Sorted Arrays.cpp
@@ -1,16 +1,47 @@
-class Solution {
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
 public:
-    int removeDuplicates(vector<int>& nums) {
-        int count = 0; 
+    int removeDuplicates(vector<int> &nums)
+    {
+        int count = 0;
         int n = nums.size();
-        for(int i = 0; i<nums.size(); i++){
-            if (i<(n-1) && nums[i] == nums[i+1])
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (i < (n - 1) && nums[i] == nums[i + 1])
                 continue;
-            else{
+            else
+            {
                 nums[count] = nums[i];
                 count++;
             }
         }
-    return count;
+        return count;
     }
 };
+
+int main()
+{
+    // Input array with duplicates (sorted as per the problem requirements)
+    vector<int> nums = {1, 1, 2, 2, 3, 3, 4, 4, 5};
+
+    Solution solution;
+
+    // Call removeDuplicates function
+    int newLength = solution.removeDuplicates(nums);
+
+    // Output the modified array and its new length
+    cout << "Array after removing duplicates: ";
+    for (int i = 0; i < newLength; i++)
+    {
+        cout << nums[i] << " ";
+    }
+    cout << endl;
+
+    cout << "New Length: " << newLength << endl;
+
+    return 0;
+}
