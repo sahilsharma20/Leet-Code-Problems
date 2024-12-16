beb9e71d67e692636ea6f57c7ca2a1306cb08cdc
sahilsharma20 <sahilsharma68018@gmail.com>
2024-12-16 10:31:05 +0530

    Quick update to Two_sum.cpp

--- Arrays/Two_sum.cpp
+++ Arrays/Two_sum.cpp
@@ -1,18 +1,50 @@
-class Solution {
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
 public:
-    vector<int> twoSum(vector<int>& arr, int x) {
-        vector <int> v(2);
+    vector<int> twoSum(vector<int> &arr, int x)
+    {
+        vector<int> v(2);
         int n = arr.size();
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if (arr[i]+arr[j]==x){
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (arr[i] + arr[j] == x)
+                {
                     v[0] = i;
                     v[1] = j;
-                    return v;
-
+                    return v; // Return as soon as a valid pair is found
                 }
             }
         }
-     return v;
+        // If no pair is found, return an empty vector
+
+        return v;
     }
 };
+
+int main()
+{
+    // Input array and target sum
+    vector<int> arr = {2, 7, 11, 15};
+    int target = 9;
+
+    // Create an instance of Solution and call the twoSum function
+    Solution solution;
+    vector<int> result = solution.twoSum(arr, target);
+
+    // Display the result
+    if (!result.empty())
+    {
+        cout << "Indices: " << result[0] << ", " << result[1] << endl;
+    }
+    else
+    {
+        cout << "No solution found" << endl;
+    }
+    return 0;
+}
