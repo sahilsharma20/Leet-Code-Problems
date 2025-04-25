61eefc1743315f6108a42d1046d98a6a270f3736
Looplord <137672866+sahilsharma20@users.noreply.github.com>
2025-04-25 20:24:01 +0530

    Solution for problem no - 2845

--- 2845_Count_of_Interesting_Subarrays.cpp
+++ 2845_Count_of_Interesting_Subarrays.cpp
@@ -1,15 +1,17 @@
 class Solution {
-    public:
-        int countInterestingSubarrays(vector<int>& nums, int modulo, int k) {
-            int res = 0;
-    
-            for (int i = 0; i < nums.size(); i++) {
-                int cnt = 0;
-                for (int j = i; j < nums.size(); j++) {
-                    if (nums[j] % modulo == k) cnt++;
-                    if (cnt % modulo == k) res++;
-                }
-            }
-            return res;
+public:
+    long long countInterestingSubarrays(vector<int>& nums, int modulo, int k) {
+        const int n=nums.size();
+        unordered_map<int, int> mp;
+        mp[0]=1;
+        mp.reserve(n);
+        long long ans=0;
+        int prefix=0;
+        for(int i=0; i<n; i++){
+            prefix+=((nums[i]%modulo)==k);
+            ans+=mp[(prefix+modulo-k)%modulo];
+            mp[prefix%modulo]++;
         }
-    };
+        return ans;
+    }
+};
